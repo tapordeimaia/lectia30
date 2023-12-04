0e79c4ba290700a75ff212d6017a6e69bfaf90e7
tapordeimaia <146013799+tapordeimaia@users.noreply.github.com>
2023-12-04 20:22:17 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("Key1", "Andrei");
+        map.put("Key2", "Ion");
+        map.put("Key3", "Vlad");
+        Service service = new Service();
+        service.afisare(map, (k, v) -> System.out.println("Key: "+ k+ " Value: "+ v));
+
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("Key1", "Andrei");
+        map2.put("Key2", "Ion");
+        map2.put("Key3", "Vlad");
+        Service<String, String> service2 = new Service<>();
+        service2.filter(map2, (k,v) -> k.contains("e") || v.contains("e"));
+    }
+}
